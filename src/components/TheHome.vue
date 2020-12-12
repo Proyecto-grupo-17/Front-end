64a82f836048659eb90c8e367d291e8869b47f1e
danfmartinez <danfmartinezviz@unal.edu.co>
2020-12-12 10:28:29 -0500

    first commit

---
+++ src/components/TheHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand">Navbar</a>
+      <form class="form-inline">
+        <button
+          class="btn btn-outline-info my-2 my-sm-0"
+          type="submit"
+          @click.prevent="logoutUser"
+        >
+          salir
+        </button>
+      </form>
+    </nav>
+    <ul class="list-group">
+      <li class="list-group-item"><span>Name: </span> {{ user.nombre }}</li>
+      <li class="list-group-item"><span>Email: </span> {{ user.email }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methos: {
+    logOutUser() {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      this.$router.push("/");
+    },
+    getUserData() {
+      let token = localStorage.getItem("jwt");
+      let user = localStorage.getItem("user");
+      this.user = JSON.parce(user);
+    },
+  },
+  created() {
+    this.getUserData();
+  },
+};
+</script>
---
+++ src/components/auth/TheLogin.vue
@@ -0,0 +1,67 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        v-model="login.email"
+      />
+      <small id="emailHelp" class="form-text text-muted"
+        >We'll never share your email with anyone else.</small
+      >
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="exampleInputPassword1"
+        v-model="login.password"
+      />
+    </div>
+    <pre>
+        {{ login }}
+    </pre>
+    <button type="submit" class="btn btn-primary" @click.prevent="loginUser">
+      Sign In
+    </button>
+  </form>
+</template>
+
+<script>
+import swal from 'sweetalert';
+export default {
+  data() {
+    return {
+      login: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async loginUser() {
+      try {
+        //console.log(this.login);
+        let response = await this.$http.post('/api/usuario/login', this.login);
+        console.login(response.data);
+        let token = response.data.tokenReturn;
+        let user = response.data.user;
+
+        localStorage.setItem('jwt', token);
+        localStorage.setItem('user',JSON.stringify(user))
+        if (token){
+          swal("Login correcto", "Los datos son correctos", "seccess");
+          this.$router.push('/home');
+        }
+      } catch (e) {
+        //console.log(error);
+        swal("Oops!","Algo salio mal", "error");
+      }
+    },
+  },
+};
+</script>
